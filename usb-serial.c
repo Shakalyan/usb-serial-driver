2770437e8e9e92876ee3453f763367d3142f7f68
shakalyan <vdovitg@gmail.com>
2024-03-25 19:12:37 +0300

    usb probe

--- usb-serial.c
+++ usb-serial.c
@@ -1,18 +1,49 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/usb.h>
 
-MODULE_LICENSE("GPL");
+#define PDEBUG(fmt, ...) printk("USB-SERIAL [%s]: " fmt, __func__, ##__VA_ARGS__)
+
+static struct usb_device_id us_table[] = 
+{
+    {USB_DEVICE(0x1a86, 0x7523)},
+    {}
+};
+MODULE_DEVICE_TABLE(usb, us_table);
+
+
+static int us_probe(struct usb_interface *interface, const struct usb_device_id *device_id)
+{
+    PDEBUG("us_probe");
+    return 0;
+}
+
+static void us_disconnect(struct usb_interface *interface)
+{
+    PDEBUG("us_disconnect");
+}
+
+
+static struct usb_driver us_driver = 
+{
+    .name = "usb_serial_driver",
+    .id_table = us_table,
+    .probe = us_probe,
+    .disconnect = us_disconnect
+};
 
 static int __init us_init(void)
 {
-    printk(KERN_DEBUG "usb-serial init");
-    return 0;
+    PDEBUG("init");
+    return usb_register(&us_driver);
 }
 module_init(us_init);
 
 static void __exit us_exit(void)
 {
-    printk(KERN_DEBUG "usb-serial exit");
+    usb_deregister(&us_driver);
 }
 module_exit(us_exit);
+
+MODULE_LICENSE("GPL");
